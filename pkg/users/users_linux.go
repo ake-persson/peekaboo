b2f3bcf8615d1f06e170800f3aea85bab42a9310
Michael Persson <michael.ake.persson@gmail.com>
2020-03-10 08:37:28 +0100

    add get users for linux

---
+++ pkg/users/users_linux.go
@@ -0,0 +1,44 @@
+// +build linux
+
+package users
+
+import (
+	"io/ioutil"
+	"strconv"
+	"strings"
+
+	"github.com/peekaboo-labs/peekaboo/pkg/pb/v1/resources"
+)
+
+func GetUsers() (*resources.UserList, error) {
+	b, err := ioutil.ReadFile("/etc/passwd")
+	if err != nil {
+		return nil, err
+	}
+
+	users := &resources.UserList{}
+	for _, l := range strings.Split(string(b), "\n") {
+		a := strings.Split(l, ":")
+
+		uid, err := strconv.ParseUint(a[2], 10, 64)
+		if err != nil {
+			return nil, err
+		}
+
+		gid, err := strconv.ParseUint(a[3], 10, 64)
+		if err != nil {
+			return nil, err
+		}
+
+		users.Users = append(users.Users, &resources.User{
+			Username:  a[0],
+			Uid:       uid,
+			Gid:       gid,
+			Name:      a[4],
+			Directory: a[5],
+			Shell:     a[6],
+		})
+	}
+
+	return users, nil
+}
