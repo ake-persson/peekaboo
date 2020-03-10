fc232f4dd0b30937d8a34923408a33fe9490fba4
Michael Persson <michael.ake.persson@gmail.com>
2020-03-10 13:51:58 +0100

    improve darwin get users

--- pkg/user/user_darwin.go
+++ pkg/user/user_darwin.go
@@ -20,36 +20,44 @@ func ListUsers() (*services.ListUsersResponse, error) {
 
 	users := &services.ListUsersResponse{Users: []*resources.User{}}
 	u := &resources.User{}
+	i := 0
 	for _, l := range strings.Split(string(out), "\n") {
 		kv := strings.SplitN(l, ": ", 2)
 		switch kv[0] {
 		case "name":
 			u.Username = kv[1]
+			i++
 		case "password":
 			continue
 		case "uid":
-			i, err := strconv.ParseInt(kv[1], 10, 64)
+			signed, err := strconv.ParseInt(kv[1], 10, 64)
 			if err != nil {
 				return nil, err
 			}
-			u.UidSigned = i
+			u.UidSigned = signed
+			i++
 		case "gid":
-			i, err := strconv.ParseInt(kv[1], 10, 64)
+			signed, err := strconv.ParseInt(kv[1], 10, 64)
 			if err != nil {
 				return nil, err
 			}
-			u.GidSigned = i
+			u.GidSigned = signed
+			i++
 		case "dir":
 			u.Directory = kv[1]
+			i++
 		case "shell":
 			u.Shell = kv[1]
+			i++
 		case "gecos":
 			u.Description = kv[1]
+			i++
 		case "":
-			if u.Username != "" {
+			if i >= 6 {
 				users.Users = append(users.Users, u)
-				u = &resources.User{}
 			}
+			u = &resources.User{}
+			i = 0
 		default:
 			return nil, fmt.Errorf("unknown key: %s", kv[0])
 		}
