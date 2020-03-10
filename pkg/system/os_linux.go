a0e557fdaffe0a7ccd7cc7c9f5a3965f260108e7
Michael Persson <michael.ake.persson@gmail.com>
2020-03-10 13:42:16 +0100

    fix linux system info

--- pkg/system/os_linux.go
+++ pkg/system/os_linux.go
@@ -3,34 +3,36 @@
 package system
 
 import (
-	"fmt"
+	"os/exec"
 	"strings"
 
 	"github.com/peekaboo-labs/peekaboo/pkg/pb/v1/resources"
 )
 
 func getOs(s *resources.System) error {
-	descr, err := readFile("/etc/redhat-release")
+	out, err := exec.Command("lsb_release", "-a").Output()
 	if err != nil {
 		return err
 	}
 
-	s.OsDescr = strings.TrimSpace(string(descr))
+	for _, l := range strings.Split(string(out), "\n") {
+		kv := strings.SplitN(l, ":\t", 2)
 
-	a := strings.SplitN(s.OsDescr, " release ", 2)
-	if len(a) != 2 {
-		return fmt.Errorf("unknown format in [%s]: %s", "/etc/redhat-release", s.OsDescr)
+		if len(kv) < 2 {
+			continue
+		}
+
+		switch kv[0] {
+		case "Distributor ID":
+			s.Os = kv[1]
+		case "Description":
+			s.OsDescr = kv[1]
+		case "Release":
+			s.OsVersion = kv[1]
+		case "Codename":
+			s.OsBuild = kv[1]
+		}
 	}
-
-	s.Os = strings.Replace(a[0], " Linux", "", 1)
-
-	a = strings.SplitN(a[1], " ", 2)
-	if len(a) != 2 {
-		return fmt.Errorf("unknown format in [%s]: %s", "/etc/redhat-release", s.OsDescr)
-	}
-
-	s.OsVersion = a[0]
-	s.OsBuild = a[1][1 : len(a[1])-1]
 
 	return nil
 }
