adae23e9420ef5446350fa78e11027ea56d7d07d
Michael Persson <michael.ake.persson@gmail.com>
2020-03-11 08:44:26 +0100

    add linux filesystems

--- pkg/storage/filesystem_linux.go
+++ pkg/storage/filesystem_linux.go
@@ -1,4 +1,4 @@
-// +build darwin
+// +build linux
 
 package storage
 
@@ -12,7 +12,7 @@ import (
 )
 
 func ListFilesystems() (*services.ListFilesystemsResponse, error) {
-	out, err := exec.Command("df", "-k", "-l", "-i").Output()
+	out, err := exec.Command("df", "-k", "-l", "--output=source,fstype,size,used,avail,itotal,iused,iavail,target").Output()
 	if err != nil {
 		return nil, err
 	}
@@ -24,39 +24,43 @@ func ListFilesystems() (*services.ListFilesystemsResponse, error) {
 		}
 
 		a := strings.Fields(l)
-		if len(a) < 6 {
+		if len(a) < 8 {
 			continue
 		}
 
 		f := &resources.Filesystem{
 			Filesystem: a[0],
+			Type:       a[1],
 			MountedOn:  a[8],
 		}
 
 		var err error
-		if f.SizeKb, err = strconv.ParseUint(a[1], 10, 64); err != nil {
+		if f.SizeKb, err = strconv.ParseUint(a[2], 10, 64); err != nil {
 			return nil, err
 		}
 
-		if f.UsedKb, err = strconv.ParseUint(a[2], 10, 64); err != nil {
+		if f.UsedKb, err = strconv.ParseUint(a[3], 10, 64); err != nil {
 			return nil, err
 		}
 
-		if f.FreeKb, err = strconv.ParseUint(a[3], 10, 64); err != nil {
+		if f.FreeKb, err = strconv.ParseUint(a[4], 10, 64); err != nil {
 			return nil, err
 		}
 
 		f.UsedPct = float32(f.UsedKb) / float32(f.SizeKb) * 100
 
-		if f.InodesUsed, err = strconv.ParseUint(a[5], 10, 64); err != nil {
+		if f.Inodes, err = strconv.ParseUint(a[5], 10, 64); err != nil {
 			return nil, err
 		}
 
-		if f.InodesFree, err = strconv.ParseUint(a[6], 10, 64); err != nil {
+		if f.InodesUsed, err = strconv.ParseUint(a[6], 10, 64); err != nil {
 			return nil, err
 		}
 
-		f.Inodes = f.InodesUsed + f.InodesFree
+		if f.InodesFree, err = strconv.ParseUint(a[7], 10, 64); err != nil {
+			return nil, err
+		}
+
 		f.InodesUsedPct = float32(f.InodesUsed) / float32(f.Inodes) * 100
 
 		resp.Filesystems = append(resp.Filesystems, f)
