76975b6790621b004a1143c11266ea60664e5274
Michael Persson <michael.ake.persson@gmail.com>
2020-03-20 11:33:11 +0100

    cleanup help

--- client/client.go
+++ client/client.go
@@ -74,11 +74,6 @@ func usage() {
   address
         Address to agent specified as <address[:port]> (default port 17711)
 `)
-	keys := []string{}
-	for k := range colorNames {
-		keys = append(keys, k)
-	}
-	fmt.Printf("\nAllowed colors:\n%s\n", strings.Join(keys, ", "))
 }
 
 func main() {
@@ -92,9 +87,11 @@ func main() {
 	flag.StringVar(&conf.KeyFile, "key-file", "~/certs/srv.key", "Server TLS key file")
 	flag.StringVar(&conf.CAFile, "ca-file", "~/certs/root_ca.crt", "CA certificate file, required for Mutual TLS")
 	flag.StringVar(&conf.Format, "fmt", "json", "Output format [json,csv,table,vtable]")
-	flag.StringVar(&conf.FormatColors, "fmt-cols", "light-cyan,light-yellow,cyan,yellow",
-		"Comma separated list of custom colors [header hostname,content hostname,headers,content]")
-	flag.BoolVar(&conf.NoColor, "no-cols", false, "Output no colors")
+	flag.StringVar(&conf.FormatColors, "colors", "light-cyan,light-yellow,cyan,yellow",
+		"Comma separated list of output colors [black,red,green,yellow,blue,magenta,cyan,light-gray,\n"+
+			"dark-gray,light-red,light-green,light-yellow,light-blue,light-magenta,light-cyan,white]\n\n"+
+			"hostname header,hostname content,headers,content")
+	flag.BoolVar(&conf.NoColor, "no-color", false, "No color output")
 	flag.StringVar(&conf.Fields, "fields", "", "Comma separated list of fields to output")
 	flag.BoolVar(&printVersion, "version", false, "Version")
 	flag.Parse()
